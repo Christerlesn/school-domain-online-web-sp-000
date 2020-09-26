749bb2a91be84d0a720e4fd76c6c1cb252f64978
Christerle Nounoute <christerlesn@gmail.com>
2020-09-26 04:37:06 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,16 +1,31 @@
 class School
 
-  attr_accessor :name, :roster
+attr_accessor :name, :roster
 
-    def initialize(name)
-      @name = name
-      @roster = {}
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, level)
+    roster[level] ||= []
+    roster[level] << student
+  end
+  
+  def sort 
+    roster.sort_by do |x, y| 
+      x 
+    end 
+  end 
+
+  def sort
+    new_hash = {}
+    roster.each do |student, grade|
+      new_hash[student] = grade.sort
+    end
+    new_hash
     end
 
-    def add_student(student, level)
-      roster[level] ||= []
-      roster[level] << student
-    end
-
+  end 
 
 end
