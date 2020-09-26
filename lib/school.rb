d5eabb1cab4ef1c4700757ebb22433195333ec3f
Christerle Nounoute <christerlesn@gmail.com>
2020-09-26 03:55:49 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+class School
+
+def initalize
+  roster = {}
+  roster.length
+end
+
+# def roster=(roster)
+#   roster = {}
+#   @roster = roster
+# end
+#
+# def roster
+#   @roster
+# end
+
+# def add_student=(student, )
+#
+# end
+
+end
